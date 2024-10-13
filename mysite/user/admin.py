7caefb7d96f38e02f6299644009382818f5085de
Krushovice <kickstar69@yandex.ru>
2024-10-13 11:57:26 +1000

    add filters to users group in admin panel

--- mysite/user/admin.py
+++ mysite/user/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["username", "email"]
+    list_filter = ["created_at", "updated_at"]
+    search_fields = ["username"]
+    date_hierarchy = "created_at"
+    ordering = ["username"]
+    show_facets = admin.ShowFacets.ALWAYS
