ba5b044108a801bfa27b2da770582baf21bc3f8b
Krushovice <kickstar69@yandex.ru>
2024-10-13 13:57:25 +1000

    register posts urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from . import views
+
 
 urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
     path("admin/", admin.site.urls),
+    path("posts/", include("mysite.post.urls", namespace="post")),
 ]
