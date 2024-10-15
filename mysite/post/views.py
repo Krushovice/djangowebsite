6615ce4a2712e35290e99be39ba391f16b0e0f14
Krushovice <kickstar69@yandex.ru>
2024-10-15 19:07:23 +1000

    create email post share view

--- mysite/post/views.py
+++ mysite/post/views.py
@@ -3,6 +3,8 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Post
 
+from .forms import EmailPostForm
+
 
 # Create your views here.
 def index(request):
@@ -41,4 +43,30 @@ def post_detail(
         request,
         "posts/post_detail.html",
         {"post": post},
+    )
+
+
+def post_share(request, post_pk):
+    # Retrieve post by id
+    post = get_object_or_404(
+        Post,
+        id=post_pk,
+        status=Post.Status.PUBLISHED,
+    )
+    if request.method == "POST":
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            # ... send email
+    else:
+        form = EmailPostForm()
+    return render(
+        request,
+        "blog/post/share.html",
+        {
+            "post": post,
+            "form": form,
+        },
     )
