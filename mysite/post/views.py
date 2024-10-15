080f22c8409f98a11cd4b913c6b0762234d881f1
Krushovice <kickstar69@yandex.ru>
2024-10-15 18:12:38 +1000

    add seo-friendly urls for posts pages

--- mysite/post/views.py
+++ mysite/post/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render, get_object_or_404
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Post
 
 
 # Create your views here.
 def index(request):
-    posts = Post.published.all()
+    posts_list = Post.published.all()
+    paginator = Paginator(posts_list, 5)
+    page_number = request.GET.get("page", 1)
+    try:
+        posts = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(
         request,
         "posts/index.html",
@@ -14,11 +22,20 @@ def index(request):
     )
 
 
-def post_detail(request, pk):
+def post_detail(
+    request,
+    year,
+    month,
+    day,
+    post,
+):
     post = get_object_or_404(
         Post,
-        pk=pk,
+        slug=post,
         status=Post.Status.PUBLISHED,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
     )
     return render(
         request,
