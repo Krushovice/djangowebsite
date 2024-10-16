6d0e6fa8ac172bba465d54cd6ce46413cb949eaf
Krushovice <kickstar69@yandex.ru>
2024-10-16 16:56:08 +1000

    fix names apps views

--- mysite/post/urls.py
+++ mysite/post/urls.py
@@ -5,10 +5,10 @@ from . import views
 app_name = "post"
 
 urlpatterns = [
-    path("", views.index, name="posts_index"),
+    path("", views.index, name="index"),
     path(
         "<int:year>/<int:month>/<int:day>/<slug:post>/",
         views.post_detail,
-        name="post_detail",
+        name="detail",
     ),
 ]
--- mysite/user/urls.py
+++ mysite/user/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = "user"
 
 urlpatterns = [
-    path("", views.index, name="users_index"),
-    path("profile/<int:pk>", views.get_profile, name="user_profile"),
+    path("", views.index, name="index"),
+    path("profile/<int:pk>", views.get_profile, name="profile"),
 ]
